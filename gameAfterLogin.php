96799f9fc26b150b4b3ac9ce79bac5497d27fae4
Maya Wallach <52044992+wallachmaya@users.noreply.github.com>
2022-08-24 03:28:21 -0400

    Update gameAfterLogin.php

--- gameAfterLogin.php
+++ gameAfterLogin.php
@@ -3,24 +3,26 @@ session_start();
     include("connection.php");
     include("functions.php");
     $user_data = check_login($con);
+    if (isset($_COOKIE['highscore'])) {
+        echo 'High score has been saved.';
+    }
+    $high = $_COOKIE['highscore'];
+    $name = $user_data['user_name'];
+    $query = "UPDATE users SET high_score = '$high' WHERE user_name = '$name'";
+    mysqli_query($con, $query);
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-<title>Benus</title>
+<title>Vroom</title>
 <h1>Not Great Car Game That I Made in A Weekend</h1>
+<link rel="stylesheet" type="text/css" href="gameCSS.css">
 </head>
 <body>
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-<style>
-canvas {
-    border:1px solid #d3d3d3;
-    background-color: #f1f1f1;
-}
-</style>
-<script type="text/javascript" src="gameJS.js"></script>  
-<button onClick="startGame();">Play</button>
-<button onClick="window.location.reload();">Restart</button>
-<a href="logout.php"><button>Logout</button></a>
+<script type="text/javascript" src="gameJS.js"></script>
+<button onClick="startGame();" class = "button">Play</button>
+<button onClick="window.location.reload();" class="button1">Restart</button>
+<a href="logout.php"><button class = "button2">Logout</button></a>
 </body>
 </html>
